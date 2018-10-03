1fe31f9a9a45f785364a15fc292ad8b1eb5915b8
bradenbest <bradentbest@gmail.com>
2018-10-03 13:55:29 -0600

    MadLibber v0.2
    + added user input

--- ChapterTwo/src/MadLibber.java
+++ ChapterTwo/src/MadLibber.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 /**
  * Gets words from user to fill in the gaps in a story.
  * @author Braden Best
- * @version 0.1
+ * @version 0.2 (ahead of assignment by 1 commit)
  * @date 2018-09-16
  *
+ * changelog:
+ *   0.2
+ *     + added user input, as I hadn't seen the detail about the *user* entering these things.
+ *   0.1
+ *     initial assignment. This is the version uploaded on canvas
  */
 public class MadLibber {
 	/* 
@@ -25,18 +31,45 @@ public class MadLibber {
 	 *    
 	 *    Be sure to double check the spacing on your output!
 	 */
-	public static void main(String[] args) {
-		String[] adjectives = {"moist", "salty"};
-		String[] nouns = {"goose", "cult", "Kool-aid"};
-		String number = "a billion";
-		String verb = "drink";
-		String bodypart = "pancreas";
+    private static Scanner scanner;
+
+	public static
+    void
+    main(String[] args)
+    {
+		String[] adjectives = new String[2];
+        String[] nouns = new String[3];
+		String number, verb, bodypart;
 		
+        scanner = new Scanner(System.in);
+
+        System.out.println("[MadLibber] I'm gonna ask you to enter a few words...");
+        adjectives[0] = get_string("[1/8] an adjective (1)");
+        adjectives[1] = get_string("[2/8] an adjective (2)");
+        nouns[0]      = get_string("[3/8] a noun (1)");
+        nouns[1]      = get_string("[4/8] a noun (2)");
+        nouns[2]      = get_string("[5/8] a noun (3)");
+        number        = get_string("[6/8] a number");
+        verb          = get_string("[7/8] a verb");
+        bodypart      = get_string("[8/8] a bodypart");
+
+        scanner.close();
 		madlib(adjectives, nouns, number, verb, bodypart);
 
 	}
+
+    private static
+    String
+    get_string(String prompt)
+    {
+        System.out.print(prompt + " >");
+        return scanner.next();
+    }
 	
-	private static void madlib(String[] adjectives, String[] nouns, String number, String verb, String bodypart){
+	private static
+    void
+    madlib(String[] adjectives, String[] nouns, String number, String verb, String bodypart)
+    {
 		System.out.printf("Hey baby, I guarantee there'll be %s times. I guarantee that at some\n", adjectives[0]);
 		System.out.printf("%s, %s or both of us is gonna want to get out of this %s. But\n", nouns[0], number, nouns[1]);
 		System.out.printf("I also guarantee that if I don't ask you to be %s, I'll %s it for the\n", adjectives[1], verb);
